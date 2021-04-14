ecd4d6c341813a5d0f8cd478751bd59b2cbc46b1
Omar Khodr <omar.khoder2000@gmail.com>
2021-04-14 21:15:29 +0300

    Add Report appointment_id PATCH request

--- api/Report.py
+++ api/Report.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify, abort
-from models.Report import Report, report_schema
-from models.User import User
 from Auth import extract_auth_token, decode_token
 import jwt
+from models.Report import Report, report_schema
+from models.User import User
 
 app_report = Blueprint('app_report', __name__)
 
 
-@app_report.route('/report', methods=['POST'])
+@app_report.route('/reports', methods=['POST'])
 def create_report():
     token = extract_auth_token(request)
     user_id = None
@@ -30,3 +30,25 @@ def create_report():
     db.session.commit()
 
     return jsonify(report_schema.dump(report))
+
+@app_report.route('/reports/AppointmentID', methods=['PATCH'])
+def change_report_appointment_id():
+    token = extract_auth_token(request)
+    user_id = None
+    if token is not None:
+        try:
+            user_id = decode_token(token)
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+            abort(403)
+
+    user = User.query.filter_by(id=user_id).first()
+    if not user.isDoctor:
+        abort(403)
+    
+    report = Report.query.filter_by(id=request.json['request_id']).first()
+    report.appointment_id = request.json['appointment_id']
+
+    db.session.commit()
+
+    return jsonify(report_schema.dump(report))
+    
